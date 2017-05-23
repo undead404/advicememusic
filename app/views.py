9b492b388b5ba6367090b9c246bcf899cfa3620c
undead404 <brute18@gmail.com>
2017-05-23 23:59:56 +0300

    pprint debug added

--- app/views.py
+++ app/views.py
@@ -2,6 +2,7 @@ from app import app
 from flask import render_template, send_from_directory
 import json
 from get_cult_artist import get_cult_artist
+from pprint import pprint
 
 @app.route('/')
 def index():
@@ -9,7 +10,9 @@ def index():
 
 @app.route('/get')
 def get_artist():
-    return json.dumps(get_cult_artist())
+    cult_artist = get_cult_artist()
+    pprint(cult_artist)
+    return json.dumps(cult_artist)
 
 @app.route('/static/<path:path>')
 def get_static(path):
